61bef24a479f133e7cb09e898d7896e159e4e2b0
alvaromorenomad <alvaromorenomad@gmail.com>
2025-12-02 20:48:03 +0100

    cambios documentos

--- Clase1/script.js
+++ Clase1/script.js
@@ -1,3 +1,5 @@
+'use strict'
+
 console.log('Archivo cargado desde fuera')
 
 let cajas_almacen;
@@ -6,6 +8,9 @@ cajas_almacen = 190;
 
 cajas_almacen = 154;
 
-console.log ('Numero cajas')
-console.log (cajas_almacen)
+console.log ('Numero cajas') // pinta la cadena de texto
+console.log (cajas_almacen) // pinta la variable
 
+function escribe_por_consola(){
+    console.log('Botón clicado');
+}
