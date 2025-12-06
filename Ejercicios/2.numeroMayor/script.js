9c5d2fe82b73c4a91b5148f14fbc6373848e18b0
alvaromorenomadariaga <alvaromorenomad@gmail.com>
2025-12-06 13:10:51 +0100

    Ejerccicio 2 a falta de la mejora

---
+++ Ejercicios/2.numeroMayor/script.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const primer_numero = Number(prompt('Introduce un número'));
+const segundo_numero = Number(prompt ('Introduce otro número'));
+console.log(typeof(primer_numero), typeof(segundo_numero))
+const nodoContenedor = document.querySelector ('#contenedor');
+
+if (primer_numero > segundo_numero){
+    nodoContenedor.innerHTML = `El número 1: (${primer_numero}) es mayor que el número 2: (${segundo_numero})`;
+}else if(primer_numero < segundo_numero){
+    nodoContenedor.innerHTML = `El número 1: (${primer_numero}) es menor que el número 2: (${segundo_numero})`;
+}else if(primer_numero === segundo_numero){
+    nodoContenedor.innerHTML = `El número 1: (${primer_numero}) es igual que el número 2: (${segundo_numero})`;
+}else{nodoContenedor.innerHTML = 'Alguno de los datos introducidos no son válidos. Asegúrate de que el valor introducido es un número y que no has añadido espacios'};
