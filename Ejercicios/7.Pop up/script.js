cb108b135d5f397c1d60cb471622974a1cc6b323
alvaromorenomad <alvaromorenomad@gmail.com>
2026-01-07 11:54:18 +0100

    ejercicio7.1

--- Ejercicios/7.Pop up/script.js
+++ Ejercicios/7.Pop up/script.js
@@ -1,21 +1,28 @@
 'use strict'
 
 
-//const nodoAbrirPopUp = document.querySelector ('#botonPopup');
-//
-//nodoAbrirPopUp.addEventListener('click', ()=> {
-//    
-//    const nodoPopUp = document.querySelector ('#pop-up');
-//
-//    nodoPopUp.classList.remove('oculto');
-//})
-
-function AbrirPopUp (){
-    const nodoPopUp = document.querySelector ('#pop-up');
+const nodoAbrirPopUp = document.querySelector ('#botonPopup');
+const nodoPopUp = document.querySelector ('#pop-up');
+nodoAbrirPopUp.addEventListener('click', ()=> {
     nodoPopUp.classList.remove('oculto');
-}
+})
 
-function CerrarPopUp (){
-    const nodoPopUp = document.querySelector('#pop-up');
+const nodoCerrar = document.querySelector('#cerrar');
+const nodoCruz = document.querySelector ('#cruz');
+nodoCerrar.addEventListener ('click', ()=> {
     nodoPopUp.classList.add('oculto');
-}
+})
+
+nodoCruz.addEventListener ('click', ()=> {
+    nodoPopUp.classList.add('oculto');
+})
+
+//function AbrirPopUp (){
+//    const nodoPopUp = document.querySelector ('#pop-up');
+//    nodoPopUp.classList.remove('oculto');
+//}
+//
+//function CerrarPopUp (){
+//    const nodoPopUp = document.querySelector('#pop-up');
+//    nodoPopUp.classList.add('oculto');
+//}
