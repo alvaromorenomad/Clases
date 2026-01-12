e5620a23ffb58a9be5b833bde16c9a7c8943f17d
alvaromorenomad <alvaromorenomad@gmail.com>
2026-01-12 18:25:28 +0100

    segundo repaso

--- Ejercicios/plantilla_pruebas/script.js
+++ Ejercicios/plantilla_pruebas/script.js
@@ -66,3 +66,71 @@ function calcularMayor (){
 }
 
 nodoBtnCalcularMayor.addEventListener('click', calcularMayor);
+
+const nodoBtnPar = document.getElementById('btnNumeroPar');
+
+function getNumeroPar (){
+    const nodoNumeroIntroducido = document.querySelector('#numeroIntroducido');
+    const numeroIntroducido = nodoNumeroIntroducido.valueAsNumber;
+
+    let str_numero_par = `<p id="parrafo">El ${numeroIntroducido} es `
+
+    const nodoParrafoInner = document.querySelector('#parrafo');
+    
+    const nodoContenedorPar = document.getElementById('contenedorNumeroPar');
+
+    if (numeroIntroducido === 0){
+        str_numero_par = `<p id="parrafo">El número intruducido es 0</p>`;
+        nodoContenedorPar.innerHTML = str_numero_par;
+        const nodoParrafoInner = document.querySelector('#parrafo');
+        nodoParrafoInner.classList.remove(...nodoParrafoInner.classList);
+        nodoParrafoInner.classList.add('amarillo');
+    }else if (numeroIntroducido % 2 === 0){
+        str_numero_par += `PAR</p>`;
+        nodoContenedorPar.innerHTML = str_numero_par;
+        const nodoParrafoInner = document.querySelector('#parrafo');
+        nodoParrafoInner.classList.remove(...nodoParrafoInner.classList);
+        nodoParrafoInner.classList.add('verde');
+    }else if (isNaN(numeroIntroducido)){
+        str_numero_par = `<p id="parrafo">No has introducido un valor válido</p>`;
+        nodoContenedorPar.innerHTML = str_numero_par;
+        const nodoParrafoInner = document.querySelector('#parrafo');
+        nodoParrafoInner.classList.remove(...nodoParrafoInner.classList);
+        nodoParrafoInner.classList.add('rojo');
+    }else{
+        str_numero_par += `IMPAR</p>`;
+        nodoContenedorPar.innerHTML = str_numero_par;
+        const nodoParrafoInner = document.querySelector('#parrafo');
+        nodoParrafoInner.classList.remove(...nodoParrafoInner.classList);
+        nodoParrafoInner.classList.add('azul');
+    };
+}
+
+nodoBtnPar.addEventListener('click', getNumeroPar);
+
+const nodoBtnSuma = document.querySelector('#btnCalcular');
+
+function sumaImpares (){
+    const nodoInputPequeno = document.getElementById('numeroPequeno');
+    const numeroPequeno = nodoInputPequeno.valueAsNumber;
+    const nodoInputGrande = document.getElementById('numeroGrande');
+    const numeroGrande = nodoInputGrande.valueAsNumber;
+
+    let acumulador = 0;
+
+    for (let cadaNumber = numeroPequeno; cadaNumber <= numeroGrande; cadaNumber++){
+        if (cadaNumber % 2 != 0){
+            acumulador += cadaNumber;
+            console.log(acumulador);
+
+            const nodoContenedorSuma = document.querySelector('#contenedorSuma');
+
+            nodoContenedorSuma.innerHTML = `<h3>El acumulado de los números impares es ${acumulador}</h3>
+
+            `;
+
+        }
+    }
+}
+
+nodoBtnSuma.addEventListener('click', sumaImpares);
