21eb30ba70cf8c8308ec49481d989131f203bd8c
alvaromorenomad <alvaromorenomad@gmail.com>
2026-01-20 11:22:44 +0100

    UpdateEjercicio9Script.js

--- Ejercicios/9.NumerosCreciendo/script.js
+++ Ejercicios/9.NumerosCreciendo/script.js
@@ -4,15 +4,39 @@ const nodoNumeroCreciendo = document.querySelector('#numeroCreciendo');
 const nodoNumeroCreciendo2 = document.querySelector('#numeroCreciendo2');
 const nodoNumeroCreciendo3 = document.querySelector('#numeroCreciendo3');
 
-const ref_interval_1 = setInterval (function(){
-    let numero = 0;
-    for (numero = 0; numero <= 40; numero++){
-       nodoNumeroCreciendo.innerHTML = `${numero}K+`;
-       //console.log(numero);  
-    };
+let numero = 0;
+let numero1 = 0;
+let numero2 = 0;
 
-   
+const ref_interval_1 = setInterval (()=>{
+    numero++;
+    nodoNumeroCreciendo.innerHTML = `${numero}K+`;
+
+    if (numero >= 40){
+        clearInterval(ref_interval_1)
+    } 
     
-    console.log(numero);
-}, 1000);
+}, 73); 
+
+const ref_interval_2 = setInterval(()=>{
+    numero1++;        
+    nodoNumeroCreciendo2.innerHTML = `${numero1}K+`;
+
+    if(numero1 >= 70){
+        clearInterval(ref_interval_2);
+    }
+
+}, 43);
+
+const ref_interval_3 = setInterval(()=>{
+    numero2++;        
+    nodoNumeroCreciendo3.innerHTML = `${numero2}K+`
+
+    if(numero2 >= 107){
+        clearInterval(ref_interval_3);
+    }
+
+}, 28);
+
+
 
